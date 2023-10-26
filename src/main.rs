a4311bf96b4247909431a9938cbdcbef033f0dd4
tommyrharper <thomasrobertharper@gmail.com>
2023-10-26 15:44:46 +0100

    docs: remove old comment

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // let function_name = "getAnotherValue()"; // should output 0x25d805cf
-    let function_name = &args[1]; // should output 0x25d805cf
+    let function_name = &args[1];
     let hashed_data = Keccak256::digest(function_name);
 
     println!("{:#02x}{:02x}{:02x}{:02x}", hashed_data[0], hashed_data[1], hashed_data[2], hashed_data[3]);
