8867fb927149b9b769cb0a983c6f63ad6703057c
tommyrharper <thomasrobertharper@gmail.com>
2023-10-26 15:39:08 +0100

    fix: print correct output

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use sha3::{Digest, Keccak256};
+use std::env;
 
 fn main() {
-    let function_name = "getAnotherValue()"; // should output 0x25d805cf
+    let args: Vec<String> = env::args().collect();
+
+    // let function_name = "getAnotherValue()"; // should output 0x25d805cf
+    let function_name = &args[1]; // should output 0x25d805cf
     let hashed_data = Keccak256::digest(function_name);
 
-    println!("0x{:x}{:x}{:x}{:x}", hashed_data[0], hashed_data[1], hashed_data[2], hashed_data[3]);
+    println!("{:#02x}{:02x}{:02x}{:02x}", hashed_data[0], hashed_data[1], hashed_data[2], hashed_data[3]);
 }
